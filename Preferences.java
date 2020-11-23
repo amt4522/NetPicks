bb8d23735c48aa96a054c549a5b081b438d1fe85
amt4522 <amt4522@gmail.com>
2020-11-23 11:49:10 -0600

    Add files via upload

---
+++ Preferences.java
@@ -0,0 +1,57 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Preferences implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    // When this method is called, it'll change scene to Home
+    public void homeBtnPushed(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent mainPg = FXMLLoader.load(getClass().getResource("NPHome.fxml"));
+        Scene mainScene = new Scene(mainPg);
+
+        // Get stage information
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
+
+    // When this method is called, it'll change scene to Genres
+    public void genresBtnPushed(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent mainPg = FXMLLoader.load(getClass().getResource("Genres.fxml"));
+        Scene mainScene = new Scene(mainPg);
+
+        // Get stage information
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
+
+    // When this method is called, it'll change scene to Ratings
+    public void ratingsBtnPushed(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent mainPg = FXMLLoader.load(getClass().getResource("Ratings.fxml"));
+        Scene mainScene = new Scene(mainPg);
+
+        // Get stage information
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(mainScene);
+        window.show();
+    }
+}
